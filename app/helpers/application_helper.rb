34e2d4904ad334e06a35dcb313881f21e646b2b8
Vladimir Avgustov <vavgustov@gmail.com>
2018-03-22 21:10:34 +0300

    Add some boilerplate...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def render_notifications
+    notifications = []
+    flash.each do |key, value|
+      css_class = case key
+                    when 'alert'
+                      'is-danger'
+                    when 'notice'
+                      'is-warning'
+                    when 'error'
+                      'is-danger'
+                    when 'success'
+                      'is-success'
+                    else
+                      'is-info'
+                  end
+      notifications << content_tag(:div, value.html_safe, class: "notification #{css_class}")
+    end
+    notifications.join('').html_safe
+  end
+
+  def is_active_page(path)
+    current_page?(path) ? "is-active" : ""
+  end
 end
