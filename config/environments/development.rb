39c1c6fec602062ad7da9be602b283a57f41519d
Vladimir Avgustov <vavgustov@gmail.com>
2018-04-01 15:54:18 +0300

    Adjust bullet config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
+    Bullet.add_footer = true
   end
 end
