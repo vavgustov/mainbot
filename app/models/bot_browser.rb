6c98bc27b00ea78aa4176556c2494a4dafb7f32f
Vladimir Avgustov <vavgustov@gmail.com>
2018-04-17 15:02:24 +0300

    Small refactoring for BotBrowser.

--- app/models/bot_browser.rb
+++ app/models/bot_browser.rb
@@ -5,14 +5,6 @@ module BotBrowser
 
   class << self
     def download(url, js: true)
-      page = process_download(url, js)
-      raise Swarm::SpiderDownloadFailure, url if page.nil?
-      page
-    end
-
-    private
-
-    def process_download(url, js)
       DOWNLOAD_ATTEMPTS.times do
         response = js ? process_with_js(url) : process_without_js(url)
         status = SUCCESS_CODES.include?(response.try(:status)) || SUCCESS_CODES.include?(response.try(:status_code))
@@ -22,8 +14,10 @@ module BotBrowser
         end
         return response.body
       end
-      nil
+      raise Swarm::SpiderDownloadFailure, url
     end
+
+    private
 
     def process_with_js(url)
       browser = Capybara.current_session
