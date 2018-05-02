217da29dac41d139869f6fce05bc98b33676f962
Vladimir Avgustov <vavgustov@gmail.com>
2018-05-02 22:34:23 +0300

    Cleanup.

--- app/javascript/packs/facets.js
+++
@@ -1,9 +0,0 @@
-// import Vue from 'vue/dist/vue.esm'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   window.repository = new Vue({
-//     el: '#app',
-//     template: '<App/>',
-//     components: { App }
-//   })
-// })
